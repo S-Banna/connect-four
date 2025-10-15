993781d389e1b683e06b37d9a50560158bc608f5
Omar Khodor <omarkhodor@Omars-Laptop.local>
2025-10-15 08:54:33 +0300

    easybot done :)

--- easyBot.c
+++ easyBot.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include "easyBot.h"
-int easyBot(int maxcolumn, int mincolumn){
-    int rand_num= (rand() % (maxcolumn - mincolumn + 1)) + mincolumn;
+
+int easyBot(int maxcolumn, int mincolumn) {
+    int rand_num = (rand() % (maxcolumn - mincolumn + 1)) + mincolumn;
     return rand_num;
 }
