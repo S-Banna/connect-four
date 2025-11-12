8fbaa879733bba011f424a08055a21acd35507d7
S-Banna <bnsari789@gmail.com>
2025-11-12 11:02:01 +0200

    used header instead of static declaration

--- mediumBot.h
+++ mediumBot.h
@@ -2,3 +2,5 @@ int mediumBot(int**, int, int, int, int*, int);
 int findWin(int**, int, int);
 int blockWin(int**, int, int);
 int potentialWin(int**, int, int, int); 
+
+int isValidColumn(int**, int, int, int);  // taken from main
